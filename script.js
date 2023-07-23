8e3818ad72a180f0644c2a5cc8567360a33782cb
Karan Raj <karanrajgodansa2003@gmail.com>
2023-07-23 16:57:12 +0530

    add date functionality by this user can fetch news of particular date

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 let currentQuery = "sports"
-let currentPage = 1
-const fetchNews = async (page, q) => {
+let currentPage = 1;
+const today = new Date();
+const yesterday = new Date(today);
+yesterday.setDate(today.getDate() - 1);
+let currentDate = yesterday.toISOString().slice(0, 10);
+const fetchNews = async (page, q, date) => {
     console.log("Fetching News...");
 
-    const url = `http://localhost:3000/news?q=${q}&page=${page}`;
+    let url;
+    if (date) {
+        url = `http://localhost:3000/news?q=${q}&page=${page}&date=${date}`;
+    } else {
+        url = `http://localhost:3000/news?q=${q}&page=${page}`;
+    }
     console.log(url);
     var req = new Request(url);
     let a = await fetch(req)
     let response = await a.json()
     console.log(JSON.stringify(response))
-    
+
     console.log(response);
     let str = ""
     resultCount.innerHTML = response.totalResults;
@@ -28,7 +37,7 @@ const fetchNews = async (page, q) => {
     document.querySelector(".content").innerHTML = str
 };
 
-fetchNews(1, currentQuery)
+fetchNews(1, currentQuery, currentDate)
 document.getElementById('search').addEventListener("click", (e) => {
     e.preventDefault();
     let query = document.getElementById('searchInput').value;
@@ -39,31 +48,50 @@ document.getElementById('previous').addEventListener("click", (e) => {
     e.preventDefault();
     if (currentPage > 1) {
         currentPage = currentPage - 1
-        fetchNews(currentPage, currentQuery)
+        const currentDate = document.getElementById('dateInput').value;
+        fetchNews(currentPage, currentQuery, currentDate)
     }
 })
 document.getElementById('next').addEventListener("click", (e) => {
     e.preventDefault();
     currentPage = currentPage + 1;
-    fetchNews(currentPage, currentQuery)
+    const currentDate = document.getElementById('dateInput').value;
+    fetchNews(currentPage, currentQuery, currentDate)
 })
 document.getElementById('Tech').addEventListener("click", (e) => {
     e.preventDefault();
-    fetchNews(1, "Technology");
+    currentQuery = "Technology"
+    const currentDate = document.getElementById('dateInput').value;
+    fetchNews(1, currentQuery, currentDate);
 })
 document.getElementById('sports').addEventListener("click", (e) => {
     e.preventDefault();
-    fetchNews(1, "Sports");
+    currentQuery = "sports"
+    const currentDate = document.getElementById('dateInput').value;
+    fetchNews(1, currentQuery, currentDate);
 })
 document.getElementById('business').addEventListener("click", (e) => {
     e.preventDefault();
-    fetchNews(1, "Business");
+    currentQuery = "Business"
+    const currentDate = document.getElementById('dateInput').value;
+    fetchNews(1, currentQuery, currentDate);
 })
 document.getElementById('weather').addEventListener("click", (e) => {
     e.preventDefault();
-    fetchNews(1, "Weather");
+    currentQuery = "weather"
+    const currentDate = document.getElementById('dateInput').value;
+    fetchNews(1, currentQuery, currentDate);
 })
 document.getElementById('health').addEventListener("click", (e) => {
     e.preventDefault();
-    fetchNews(1, "Health");
+    currentQuery = "health"
+    const currentDate = document.getElementById('dateInput').value;
+    fetchNews(1, currentQuery, currentDate);
 })
+const dateInput = document.getElementById('dateInput');
+dateInput.addEventListener('change', (event) => {
+    const currentDate = event.target.value; // Get the selected date from the input
+
+    // Fetch news with the selected date
+    fetchNews(1, currentQuery, currentDate);
+});
